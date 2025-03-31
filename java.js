0c4554af65831a3679f3506219fda6ea1e4df532
Jeremy Winnett <winnettjeremy@gmail.com>
2025-03-31 13:05:54 -0400

    added loop to run playRound for 5 rounds

--- java.js
+++ java.js
@@ -1,6 +1,4 @@
 
-
-
 //**run the program
 //**randomize computer input to put up against user input
 
@@ -20,78 +18,67 @@ function getComputerChoice() {
 
 
 function getHumanChoice() {
-    userInput = prompt("Rock, Paper, Scissors!").toLowerCase();
-    if (userInput == "rock") {
-        return ("rock");
+    let userInput = prompt("Rock, Paper, Scissors!").toLowerCase();
+    if (userInput === "rock" || userInput === "paper" || userInput === "scissors") {
+        return userInput;
+    } else {
+        alert("Please enter a valid response");
+        return getHumanChoice();
     }
-    else if (userInput == "paper") {
-        return ("paper");
-    }
-    if (userInput == "scissors") {
-        return ("scissors");
-    } else return ("Please enter a valid response");
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
+//const humanSelection = getHumanChoice();
+//const computerSelection = getComputerChoice();
 
 //**determine the winner- display win, lose or draw
 //**establish rules, example: rock beats scissors, paper beats rock, scissors beats paper, same input is a draw
-var humanScore = 0;
-var computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanSelection;
-    computerChoice = computerSelection;
 
-    if (humanChoice === computerChoice) 
-        console.log("It's a Draw!");
-
-    if (humanChoice == "scissors" && computerChoice == "rock") {
-        computerScores();
-        console.log("You lose! rock beats scissors!")
-    }
-    if (humanChoice == "rock" && computerChoice == "paper") {
-        computerScores();
-        console.log("You lose! paper beats rock")
-    }
-    if (humanChoice == "paper" && computerChoice == "scissors") {
-        computerScores();
-        console.log("You lose! scissors beats paper")
-    }
-    if (humanChoice == "scissors" && computerChoice == "paper") {
-        humanScores();
-        console.log("You win! scissors beats paper")
-    }
-    if (humanChoice == "rock" && computerChoice == "scissors") {
-        humanScores();
-        console.log("You win! rock beats scissors")
-    }
-    if (humanChoice == "paper" && computerChoice == "rock") {
-        humanScores();
-        console.log("You win! paper beats rock")
-    }
-    function humanScores() {
-        humanScore += 1;
-    }
-    function computerScores() {
-        computerScore += 1;
-    }
-    console.log("Opponent score: " + computerScore);
-    console.log("Your score: " + humanScore);
-}
 
 function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-    
+    function playRound() {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
 
-    playRound();
+        function chooseWinner() {
+            if (humanChoice === computerChoice) {
+                return "It's a Draw!";
+            }
+
+            if (
+                (humanChoice === "scissors" && computerChoice === "rock") ||
+                (humanChoice === "rock" && computerChoice === "paper") ||
+                (humanChoice === "paper" && computerChoice === "scissors")
+            ) {
+                computerScore += 1;
+                return `You lose! ${computerChoice} beats ${humanChoice}!`;
+            } else {
+                humanScore += 1;
+                return `You win! ${humanChoice} beats ${computerChoice}!`;
+            }
+
+        }
+        console.log(chooseWinner());
+        console.log(`You chose: ${humanChoice}`);
+        console.log(`Opponent chose: ${computerChoice}`);
+        console.log(`Your score: ${humanScore}`);
+        console.log(`Opponent score: ${computerScore}`);
+
+    }
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    console.log("Game over!");
+    console.log(
+        humanScore > computerScore
+        ? "You win!"
+        : humanScore < computerScore
+        ? "You lose."
+        : "It's a tie"
+    );
 }
 
-
-
-
-console.log("Opponent chose: " + computerSelection);
-console.log("You chose: " + humanSelection);
 playGame();
